879141f379c3e2061de1bd8e7c9f111b864df99a
borimna <22000216@handong.ac.kr>
2022-05-19 17:33:27 +0900

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'appstate.dart';
 import 'profiledetail.dart';
 import 'profilelist.dart';
 import 'updateprofile.dart';
 import 'app.dart';
 import 'login.dart';
 import 'mainpage.dart';
+import 'package:provider/provider.dart';
 
 
 //Eunjin
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, _) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,15 +31,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -39,15 +40,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -61,12 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
--- lib/profilelist.dart
+++ lib/profilelist.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'addprofile.dart';
 import 'model/animal.dart';
 import 'model/animal_List.dart';
+import 'storage.dart';
 
 class ProfileList extends StatefulWidget {
   const ProfileList({Key? key}) : super(key: key);
@@ -12,155 +15,210 @@ class ProfileList extends StatefulWidget {
 }
 
 class _ProfileListState extends State<ProfileList> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Stream<QuerySnapshot> _stream = FirebaseFirestore.instance
+      .collection('animal')
+      .orderBy('name')
+      .snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('profile list'),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.add,
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => AddProfile()));
-                }),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: ListView(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-            children: _buildListCards(context),
-          ),
-        ));
-  }
-
-  List<Expanded> _buildListCards(BuildContext context) {
-    List<Animal> animals = AnimalRepository.loadAnimals(Category.all);
-
-    if (animals.isEmpty) {
-      return const <Expanded>[];
-    }
-
-    return animals.map((animal) {
-      return Expanded(
-        child: Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 5, 10, 5),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  clipBehavior: Clip.hardEdge,
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      minWidth: 100,
-                      minHeight: 100,
-                      maxWidth: 100,
-                      maxHeight: 100,
-                    ),
-                    child: Image.network(
-                      animal.image,
-                      fit: BoxFit.fill,
-                    ),
+      appBar: AppBar(
+        title: Text('profile list'),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.add,
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddProfile()));
+              }),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _stream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    padding: const EdgeInsets.all(16.0),
+                    childAspectRatio: 8.0 / 9.0,
+                    children:
+                    snapshot.data!.docs.map((DocumentSnapshot document) {
+                      return Animal(
+                        image: document.id,
+                        name: document['name'].toString(),
+                        age: document['age'].toString(),
+                        sex: document['sex'].toString(),
+                        weight: document['weight'].toString(),
+                        live: document['live'].toString(),
+                        desc: document['desc'].toString(),
+                        like: document['like'].toString(),
+                        eat: document['eat'].toString(),
+                      );
+                    }).toList(),
                   ),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(3),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxWidth: 50,
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                animal.name,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                maxLines: 1,
-                              ),
-                              Text(
-                                " / " +
-                                    animal.age.toString() +
-                                    "살 " +
-                                    animal.sex,
-                                style: const TextStyle(
-                                  fontSize: 10,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            animal.live,
-                            style: const TextStyle(
-                              fontSize: 10,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            animal.desc,
-                            style: const TextStyle(
-                              fontSize: 10,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.favorite,
-                                    color: Colors.pink,
-                                    size: 15,
-                                  ),
-                                  onPressed: () {}),
-                              Text(
-                                animal.like.toString(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.pink,
-                                ),
-                              ),
-                              IconButton(
-                                  icon: Icon(
-                                    Icons.restaurant_rounded,
-                                    color: Colors.green,
-                                    size: 15,
-                                  ),
-                                  onPressed: () {}),
-                              Text(
-                                animal.eat.toString(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.green,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+              ],
+            );
+          } else {
+            return Card();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class Animal extends StatelessWidget {
+  const Animal({
+    required this.image,
+    required this.name,
+    required this.age,
+    required this.sex,
+    required this.weight,
+    required this.live,
+    required this.desc,
+    required this.like,
+    required this.eat,
+  });
+
+  final image;
+  final name;
+  final age;
+  final sex;
+  final weight;
+  final live;
+  final desc;
+  final like;
+  final eat;
+
+  @override
+  Widget build(BuildContext context) {
+    final Storage storage = Storage();
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18 / 11,
+            child: FutureBuilder(
+                future: storage.downloadURL(image),
+                builder:
+                    (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    return Container(
+                        child: Image.network(
+                          snapshot.data!,
+                          fit: BoxFit.fill,
+                        ));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting ||
+                      !snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+
+                  return Container();
+                }),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 50,
                     ),
-                  ],
-                ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              name,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                            ),
+                            Text(
+                              " / " +
+                                  age +
+                                  "살 " +
+                                  sex,
+                              style: const TextStyle(
+                                fontSize: 10,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          live,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          desc,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                                icon: Icon(
+                                  Icons.favorite,
+                                  color: Colors.pink,
+                                  size: 15,
+                                ),
+                                onPressed: () {}),
+                            Text(
+                              like,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.pink,
+                              ),
+                            ),
+                            IconButton(
+                                icon: Icon(
+                                  Icons.restaurant_rounded,
+                                  color: Colors.green,
+                                  size: 15,
+                                ),
+                                onPressed: () {}),
+                            Text(
+                              eat,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      );
-    }).toList();
+          ),
+        ],
+      ),
+    );
   }
 }
