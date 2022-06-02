731f0b164c90ccc9a5d84fdb8e75cae1c6594a4b
gracentruth <63465350+gracentruth@users.noreply.github.com>
2022-06-03 01:51:59 +0900

    update

--- lib/addprofile.dart
+++ lib/addprofile.dart
@@ -79,15 +79,6 @@ class _AddProfileState extends State<AddProfile> {
                 // if (_formKey.currentState!.validate()) {
                 storage.uploadFile(_image!.path, _name.text + ".png");
 
-                FirebaseFirestore.instance.collection('chat').doc(_name.text).set(
-                    {
-                      'list' : [],
-                    });
-
-                setState(() {
-
-                });
-
                 FirebaseFirestore.instance
                     .collection('animal')
                     .add(<String, dynamic>{
